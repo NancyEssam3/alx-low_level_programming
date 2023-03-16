45e31010c890b6bd128b7f4a237fb46b1982b2e7
Nancy Essam <nancy.essam303@gmail.com>
2023-03-16 16:37:00 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int integer;
+	long int longinteger;
+	long long int longlonginteger;
+	char character;
+	float floatnum;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longinteger));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lonlonginteger));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatnum));
+	return (0);
+}
