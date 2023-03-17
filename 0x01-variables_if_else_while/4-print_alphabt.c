cd4dd981c8c34fa78fc1a9a645bb47cc935b8f75
Nancy Essam <nancy.essam303@gmail.com>
2023-03-17 16:47:48 -0700

    no q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+	{
+		if (lowcase != 'q' || lowcase != 'e')
+		{
+			putchar(lowcase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
