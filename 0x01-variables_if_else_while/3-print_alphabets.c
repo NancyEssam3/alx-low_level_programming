328d230b56d2231e6330652b81b601479929055f
Nancy Essam <nancy.essam303@gmail.com>
2023-03-17 16:45:41 -0700

    up & low

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,15 @@
 int main(void)
 {
 	char lowcase;
+	char upcase;
 
 	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
 	{
 		putchar(lowcase);
+	}
+	for (upcase = 'A'; upcase <= 'Z'; upcase++)
+	{
+		putchar(upcase);
 	}
 	putchar('\n');
 	return (0);
