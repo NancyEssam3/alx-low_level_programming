52c4bb22b16adae2b2d35c0f26e59a32fc950308
Nancy Essam <nancy.essam303@gmail.com>
2023-03-17 17:01:28 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowcase;
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+        {
+                putchari);
+        }
+	for (lowcase = 'a'; lowcase <= 'f'; lowcase++)
+	{
+		putchar(lowcase);
+	}
+	putchar('\n');
+	return (0);
+}
