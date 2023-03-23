ec1fdb676bfb401eb2afaa7e1c5d425e13bbb74b
Nancy Essam <nancy.essam303@gmail.com>
2023-03-23 16:20:58 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,30 +12,29 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b )
+	if (a > b)
 	{
-		if(a >c)
-		{	
+		if (a > c)
+		{
 			largest = a;
 		}
-		else 
+		else
 		{
 			largest = c;
 		}
 
 	}
-	else 
+	else
 	{
 		if (b > c)
 		{
 			largest = b;
 		}
-		else 
+		else
 		{
 			largest = c;
 		}
 	}
-	
 
 	return (largest);
 }
