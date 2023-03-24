21be8b2be747f6df581ab4d2dfddd6fa34d0b606
Nancy Essam <nancy.essam303@gmail.com>
2023-03-24 15:56:07 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,16 @@ void print_diagonal(int n)
 {
 	int i = 0;
 	int k = 0;
-	
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (k = 0; k < i; k++) 
+			for (k = 0; k < i; k++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
-		}	
+		}
 	}
 	else
 	{
