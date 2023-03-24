d4b97bfa724ab0007f08c59ccd0383197b4d91f4
Nancy Essam <nancy.essam303@gmail.com>
2023-03-24 15:29:17 -0700

    draw lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,12 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		_putchar('_');
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 
