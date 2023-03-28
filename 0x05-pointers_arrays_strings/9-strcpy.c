f2d6a6ca5f5d2e8fa803663b08ae2e8427fc42c4
Nancy Essam <nancy.essam303@gmail.com>
2023-03-27 19:21:07 -0700

    copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,6 +12,7 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	char *temp = dest;
 	while (*src != '\0')
 	{
 		*dest = *src;
@@ -19,5 +20,6 @@ char *_strcpy(char *dest, char *src)
 		src++;
 	}
 	*dest = '\0';
-	return (dest);
+
+	return (temp);
 }
