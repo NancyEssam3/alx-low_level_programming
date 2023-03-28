6004fdd95280a833a2e604bdb07a7975e1503031
Nancy Essam <nancy.essam303@gmail.com>
2023-03-27 19:18:28 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,7 @@ void print_array(int *a, int n)
 	{
 		if (i == (n - 1))
 		{
-			printf("%d\n", *(a + i));
+			printf("%d", *(a + i));
 		}
 		else
 		{
@@ -23,4 +23,5 @@ void print_array(int *a, int n)
 		}
 		i++;
 	}
+	printf("\n");
 }
