46cad90f0392cf9ac0376fc0636a63a10a5e2684
Nancy Essam <nancy.essam303@gmail.com>
2023-03-27 18:33:59 -0700

    skip one

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@ void puts2(char *str)
 	int len = 0, i = 0;
 
 	while (str[len] != '\0')
-	{	
+	{
 		len++;
 	}
 
 	for (i = 0; i < len; i = i + 2)
-	{	
+	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
