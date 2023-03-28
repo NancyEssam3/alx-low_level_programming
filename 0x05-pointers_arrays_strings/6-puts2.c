dd18e24477672f69e96eb926e09e095db1962b2c
Nancy Essam <nancy.essam303@gmail.com>
2023-03-27 18:32:57 -0700

    skip one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+	{	
+		len++;
+	}
+
+	for (i = 0; i < len; i = i + 2)
+	{	
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
