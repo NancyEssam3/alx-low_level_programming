c6142b0c7f7a02bd7ff396520f1adc50903e367b
NancyEssam3 <108620646+NancyEssam3@users.noreply.github.com>
2023-04-10 01:41:45 +0200

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * _abs - gets abs of number
+ * @r: input number
+ * Return: pos number
+ */
+
+int _abs(int r)
+{
+    if (r >= 0)
+        Return(r);
+    else
+        Return(r * -1);
+}
