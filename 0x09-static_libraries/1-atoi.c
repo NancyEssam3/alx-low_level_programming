78ff573566527760ddc2707c6004ae56405fb218
NancyEssam3 <108620646+NancyEssam3@users.noreply.github.com>
2023-04-10 01:39:26 +0200

    Create 1-atoi.c

---
+++ 0x09-static_libraries/1-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_atoi - function that convert string to integer
+ *@s: input string
+ *Return: integer
+ */
+
+int _atoi(char *s)
+{
+  
+    Return(0);
+}
