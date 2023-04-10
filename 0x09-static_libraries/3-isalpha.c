0114aeb215977f3d3cbc3e04600880277a65d02a
Nancy Essam <nancy.essam303@gmail.com>
2023-04-09 17:24:38 -0700

    betty

--- 0x09-static_libraries/3-isalpha.c
+++ 0x09-static_libraries/3-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* _isalpha - function that checks if the character is a letter, lowercase or uppercase.
+* _isalpha - function that checks if the character is a letter
+* , lowercase or uppercase.
 * @c: An input character
 * Return: 1 or 0 in otherwise.
 */
 
 int _isalpha(int c)
 {
-  return (0);
+	return (0);
 }
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -8,8 +8,8 @@
 
 int _abs(int r)
 {
-    if (r >= 0)
-        return(r);
-    else
-        return(r * -1);
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
 }
