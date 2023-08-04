3d529d655766e4d8c8b6a0782cff11975a5eb38b
Nancy Essam <nancy.essam303@gmail.com>
2023-08-04 16:53:09 -0700

    answer 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds positive nums
+ * @argc: arg count
+ * @argv: args
+ * Return: 0 , 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 1)
+	{
+
+		printf("0\n");
+	}
+	else
+	{
+		int index = 0;
+		int sum = 0;
+
+		while (index < argc)
+		{
+
+			if (!(isdigit(argv[argc][index])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = sum + atoi(argv[index]);
+			}
+			index++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
