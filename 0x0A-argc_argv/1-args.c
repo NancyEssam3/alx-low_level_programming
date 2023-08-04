a6f9f14943e5e981f157a576fd4d1ca49fde3cff
Nancy Essam <nancy.essam303@gmail.com>
2023-08-04 16:15:38 -0700

    answer 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints num of args passed
+ * @argc: args number
+ * @argv: input args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
