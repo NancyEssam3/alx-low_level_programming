211418bd7a3352797f04004ffa7b1384d3d53716
Nancy Essam <nancy.essam303@gmail.com>
2023-08-04 16:07:29 -0700

    answer 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name followed by new line
+ * @argc: input size
+ * @argv: input arguments
+ * Return: 0
+ */
+int main(int argc,char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s",argv[0]);
+		printf("\n");
+	}
+	retrun (0);
+}
