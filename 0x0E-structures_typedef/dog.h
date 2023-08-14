6d7ae2550d7c9b725a583a636f36430a2c884e1d
Nancy Essam <nancy.essam303@gmail.com>
2023-08-14 16:56:53 -0700

    answer 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef poppy_H
+#define poppy_H
+
+/**
+ * struct dog - basic dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: Dog owner
+ * Description: DS for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
