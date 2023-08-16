7d921ee38bd03c7eea5056f63f1a665c4fc1a494
Nancy Essam <nancy.essam303@gmail.com>
2023-08-16 15:06:51 -0700

    answer 0.1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * print_name - fun that prints a name
  * @name: name of person
  * @f: pointer to function
- * Return: void 
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
