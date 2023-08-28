77b16813b70407dcb6372ac993cd91a732ab6378
Nancy Essam <nancy.essam303@gmail.com>
2023-08-28 15:23:17 -0700

    answer 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
  * @head: input list
  * Return: void
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head)
+	while (*head)
 	{
-		temp = head;	
-		head = head->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
-	head = NULL;
+	*head = NULL;
 }
